2dbc4a7c0907594d2e2c213ff15a486ececc9d4c
darcy <acednes@gmail.com>
2024-05-02 18:45:03 +1000

    list of strings

--- main.c
+++ main.c
@@ -38,26 +38,69 @@ typedef struct {
 } Token;
 
 typedef struct {
-    Token *items;
+    String *items;
     int len;
     int cap;
 } TokenList;
+TokenList tokenlist_new(int cap) {
+    TokenList list;
+    list.len = 0;
+    list.cap = cap;
+    list.items = malloc(cap * sizeof(String));
+    if (!list.items) {
+        perror("malloc failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    return list;
+}
+void tokenlist_push(TokenList *list, String item) {
+    if (list->len >= list->cap) {
+        int cap = list->cap + 10;  // Genius algorithm
+        String *new_items = realloc(list->items, cap * sizeof(String));
+        if (!new_items) {
+            perror("realloc failed.\n");
+            exit(EXIT_FAILURE);
+        }
+        list->cap = cap;
+        list->items = new_items;
+    }
+    list->items[list->len] = item;
+    list->len++;
+}
 
-String parse_tokens() {
+TokenList parse_tokens() {
+    TokenList tokenlist = tokenlist_new(10);
     String string = string_new(10);
+
     int ch;
     while ((ch = getchar()) != EOF) {
-        printf("%c", ch);
-        string_push(&string, ch);
+        if (ch == '\n') {
+            if (string.len > 0) {
+                tokenlist_push(&tokenlist, string);
+                string = string_new(10);
+            }
+        } else {
+            string_push(&string, ch);
+        }
     }
-    return string;
+
+    if (string.len > 0) {
+        tokenlist_push(&tokenlist, string);
+    }
+
+    return tokenlist;
 }
 
 int main(int argc, char **argv) {
-    String string = parse_tokens();
+    TokenList tokenlist = parse_tokens();
 
-    char item;
-    for (int i = 0; (item = string.items[i]); i++) {
-        printf("%c.", item);
+    for (int i = 0; i < tokenlist.len; i++) {
+        String string = tokenlist.items[i];
+
+        for (int i = 0; i < string.len; i++) {
+            printf("%c", string.items[i]);
+        }
+
+        printf("\n---------------------\n");
     }
 }
